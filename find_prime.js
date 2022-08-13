125f699e7418a09cc49c247a889e67da5e0e3d0c
Mark <mark.n.bacon.jr@gmail.com>
2022-08-13 15:31:08 -0500

    pass

---
+++ find_prime.js
@@ -0,0 +1,20 @@
+function sumPrimes(num) {
+    let sum = 0
+  
+    // console.log(num)
+    function isPrime(num) {
+      if (num < 2) return false;
+      for (let i = 2; i < num; i++) {
+        if (num % i == 0)
+          return false
+      }
+      return true
+    }
+    for (let i = 0; i <= num; i++) {
+      if (isPrime(i)) {
+        // console.log(i)
+        sum += i
+      }
+    }
+    return (sum);
+  }

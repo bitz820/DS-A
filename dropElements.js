584af37a041b2bd04c4bd9f23482b8e43f202709
Mark <mark.n.bacon.jr@gmail.com>
2022-08-15 14:19:44 -0500

    pass

---
+++ dropElements.js
@@ -0,0 +1,12 @@
+function dropElements(arr, func) {
+
+    for(let i=0; i<arr.length; i++){
+      if (func(arr[i]) == true){
+        return arr.slice(i)
+      }
+    }
+    
+      return [];
+    }
+    
+    dropElements([1, 2, 3], function (n) { return n < 3; });
